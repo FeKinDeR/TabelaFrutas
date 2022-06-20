ac06e08b51fad642fbb3a6a146f52e2db3c2fdbe
KDR <fekindermvp@gmail.com>
2022-06-20 13:06:57 -0300

    criando tabelas
    exemplo de tabela no console.

---
+++ frutas.js
@@ -0,0 +1,16 @@
+const frutas = [
+        {
+            nome: 'Laranja',
+            quantidade: 22,
+        },
+        {
+            nome: 'Morango',
+            quantidade: 15,
+        },
+        {
+            nome: 'Uva',
+            quantidade: 4,
+        },
+    ];
+    console.table(frutas);
+
